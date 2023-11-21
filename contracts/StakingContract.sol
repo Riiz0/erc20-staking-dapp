1e305f24be3cd726285672db88a2e70216414e09
SIR <shawnrizo@hotmail.com>
2023-11-21 14:55:14 -0500

    (Chore) Worked on the StakingContract Functions, Will Be Adding Additional Functionalities

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -9,33 +9,112 @@ import "./StakedToken.sol";
 
 contract StakingContract is Ownable, Pausable, ReentrancyGuard {
     using SafeERC20 for IERC20;
-    constructor() {}
 
     //The ERC20 token that is being staked
+    IERC20 public stakedToken;
+
     //the total number of tokens staked
+    uint256 public totalStaked;
+
     //A mapping of the user address to their staked token balances
+    mapping(address => uint256) public stakedBalances;
+
+    //the staked reward rate, expressed in a precentage
+    uint256 public rewardRate;
+
+    // The last reward timestamp
+    uint256 public lastRewardTimestamp;
+
+    //an event emitted when a user stakes tokens
+    event Stake(address indexed staker, uint256 amount);
+
+    //an event emitted when a user unstakes tokens
+    event Unstake(address indexed unstaker, uint256 amount);
+
+    //an event emitted when staking rewards are distributed
+    event RewardDistribution(address indexed account, uint256 amount);
+
+    //an event emitted when the owner updates the reward rate
+    event RewardRateUpdated(uint256 newRewardRate);
+
+    constructor(address _stakedTokenAddress) {
+        stakedToken = IERC20(_stakedTokenAddress);
+    }
 
     function stake(uint256 amount) public {
         // Allows users to stake their tokens
-        //An event emitted when a user stakes tokens
+        require(amount > 0, "Staking amount must be greater than zero");
+
+        // Transfer the staked tokens from the user to the contract
+        stakedToken.safeTransferFrom(msg.sender, address(this), amount);
+
+        // Update the users staked balance and the total staked amount
+        stakedBalances[msg.sender] += amount;
+        totalStaked += amount;
+
+        //Emit the Stake Event
+        emit Stake(msg.sender, amount);
     }
 
     function unstake(uint256 amount) public {
         // Allows users to unstake their tokens
-        //An event emiited when a user unstakes tokens
+       require (amount > 0, "Unstaked amount must be greater than zero");
+       require(stakedBalances[msg.sender] >= amount, "Insufficient staked balance");
+
+        // Transfer the staked tokens from the contract to the user balance
+        stakedToken.safeTransfer(msg.sender, amount);
+
+        //Update the users Staked balance and the total staked amount
+        stakedBalances[msg.sender] -= amount;
+        totalStaked -= amount;
+
+        //Emit the Unstake Event
+        emit Unstake(msg.sender, amount);
     }
 
     function claimRewards() public {
-        // Allows users to claim their accumulated rewards
-        //An event emiited when staking rewards are distributed
+        // Calculate the accumulated rewards for the user
+        uint256 rewards = calculateRewards(msg.sender);
+
+        // Transfer the rewards to the user
+        stakedToken.safeTransfer(msg.sender, rewards);
+
+        // Update the last reward timestamp
+        lastRewardTimestamp = block.timestamp;
+
+        // Emit the RewardsDistributed event
+        emit RewardDistribution(msg.sender, rewards);
     }
 
-    function calculateRewards(address account) public{
-        // Calculates the accumulated rewards for a specific account
+    function calculateRewards(address account) public view returns (uint256) {
+        // Calculate time elapsed since last reward distribution
+        uint256 timeElapsed = block.timestamp - lastRewardTimestamp;
+
+        // Calculate annual rewards based on staked balance and reward rate
+        uint256 annualRewards = stakedBalances[account] * rewardRate / 100;
+
+        // Calculate accumulated rewards based on time elapsed and annual rewards
+        uint256 accumulatedRewards = annualRewards * timeElapsed / 365 days;
+
+        return accumulatedRewards;
     }
 
-    function setRewardRate(uint256 newRewardRate) public{
-        // Updates the reward rate for staking
-        //The staking reward rate, expressed as a percentage
+    function setRewardRate(uint256 newRewardRate) public onlyOwner {
+    // Validate the new reward rate
+    require(newRewardRate <= 100, "Reward rate cannot exceed 100%");
+
+    // Update the reward rate
+    rewardRate = newRewardRate;
+
+    // Emit an event indicating that the reward rate has been changed
+    emit RewardRateUpdated(newRewardRate);
     }
 }
+
+//Add a minimum staking period
+
+//add a withdrawal fee
+
+//dynamic reward rate
+
+//slashing mechanism to prevent malicious behavior or exploitation
---
+++ src/app/more.tsx
@@ -0,0 +1,98 @@
+import Image from 'next/image'
+import React from 'react'
+import Link from 'next/link';
+
+export default function More() {
+    return(
+        <div className="relative min-h-screen">
+        {/* Navbar */}
+        <nav className="bg-white w-full fixed top-0 left-0 p-4 flex items-center justify-between border-b border-gray-300">
+          <div className="flex items-center">
+            <Image
+              src="/logo.svg" // Replace with your logo image path
+              alt="Logo"
+              width={32}
+              height={32}
+            />
+          </div>
+          <div className="flex space-x-4">
+            <Link href="/" className="text-black hover:text-blue-500">Stake</Link>
+            <Link href="/more" className="text-black hover:text-blue-500">More</Link>
+          </div>
+          <button className="bg-black text-white px-4 py-2 rounded">
+            DApp
+          </button>
+        </nav>
+
+        {/* Main Content */}
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="relative z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+
+            
+        target="_blank"
+
+            
+        rel="noopener noreferrer"
+            >
+            </a>
+        </div>
+
+        {/* Transparent container for staking pool information */}
+        <div className="absolute top-20 left-0 w-full h-full bg-transparent">
+        <div className="container mx-auto max-w-7xl px-4">
+            <h2 className="text-3xl font-bold text-center">Staking Pool Information</h2>
+            <p className="text-lg text-center">This is the staking pool information.</p>
+        </div>
+        </div>
+        </div>
+
+            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+            </div>
+
+            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
+                <a
+                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                <h2 className={`mb-3 text-2xl font-semibold text-center`}>
+                    Stake
+                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                    -&gt;
+                    </span>
+                </h2>
+                <p className={`m-0 max-w-[50ch] text-sm opacity-50 text-center`}>
+                    "Text Will Be Here To Display The Amount Of APY For Each Stake"
+                </p>
+                </a>
+
+                <a
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                <h2 className={`mb-3 text-2xl font-semibold text-center`}>
+                    Confirm
+                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                    -&gt;
+                    </span>
+                </h2>
+                <p className={`m-0 max-w-[50ch] text-sm opacity-50 text-center`}>
+                "Text Will Be Here To Display The Confirm Button For Each Stake"
+                </p>
+                </a>
+            </div>
+            </main>
+
+            <footer className="bg-white w-full fixed bottom-0 left-0 p-4 flex items-center justify-center border-t border-gray-300">
+            <p className="text-center text-black">Staking Project Created For Your Eyes Only</p>
+            </footer>
+        </div>
+    )
+}
