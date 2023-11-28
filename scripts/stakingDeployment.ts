220a005d91b19bb06f6890ac8ce79099af239947
SIR <shawnrizo@hotmail.com>
2023-11-28 17:42:11 -0500

    (Chore) Created Token Test Script

--- scripts/stakingDeployment.ts
+++ scripts/stakingDeployment.ts
@@ -1,10 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  // Get the StakedToken contract factory
+  const StakedTokenFactory = await ethers.getContractFactory("StakedToken");
 
+  // Deploy the StakedToken contract
+  const stakedToken = await StakedTokenFactory.deploy();
+  await stakedToken.deployed();
 
+  console.log("StakedToken deployed to:", stakedToken.address);
 }
-
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
--- test/token.ts
+++ test/token.ts
@@ -1,6 +1,42 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
+describe("StakedToken", function () {
+  it("should have the correct name and symbol", async function () {
+    const StakedTokenFactory = await ethers.getContractFactory("StakedToken");
+    const stakedToken = await StakedTokenFactory.deploy();
+    await stakedToken.deployed();
 
-})
+    expect(await stakedToken.name()).to.equal("StakedToken");
+    expect(await stakedToken.symbol()).to.equal("SKTK");
+  });
+
+  it("should allow the owner to mint tokens", async function () {
+    const StakedTokenFactory = await ethers.getContractFactory("StakedToken");
+    const stakedToken = await StakedTokenFactory.deploy();
+    await stakedToken.deployed();
+
+    const owner = await stakedToken.owner();
+
+    await stakedToken.mint(owner, 100);
+    const balance = await stakedToken.balanceOf(owner);
+    expect(balance).to.equal(100);
+  });
+
+  it("should allow anyone to burn tokens", async function () {
+    const StakedTokenFactory = await ethers.getContractFactory("StakedToken");
+    const stakedToken = await StakedTokenFactory.deploy();
+    await stakedToken.deployed();
+
+    const owner = await stakedToken.owner();
+    const burner = await ethers.getSigner("0x0000000000000000000000000000000000000000");
+
+    await stakedToken.mint(owner, 100);
+
+    await stakedToken.connect(burner).burn(owner, 50);
+
+    const balance = await stakedToken.balanceOf(owner);
+
+    expect(balance).to.equal(50);
+  });
+});
